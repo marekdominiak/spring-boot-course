b50007aa65986f1c2b571678d9e8a5351641c4d6
Marek Dominiak <marek.dominiak@mintra.com>
2021-10-11 11:24:24 +0200

    Day 1st

--- src/main/java/pl/dominussoft/springbootcourse/app/infrastructure/persistence/CourseRepositorySpringData.java
+++ src/main/java/pl/dominussoft/springbootcourse/app/infrastructure/persistence/CourseRepositorySpringData.java
@@ -1,12 +1,5 @@
 package pl.dominussoft.springbootcourse.app.infrastructure.persistence;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.dominussoft.springbootcourse.app.domain.Course;
-import pl.dominussoft.springbootcourse.app.domain.CourseRepository;
-
-import java.util.UUID;
-
-@Repository
-public interface CourseRepositorySpringData extends CourseRepository, CrudRepository<Course, UUID> {
-}
+//@Repository
+//public interface CourseRepositorySpringData extends CourseRepository, CrudRepository<Course, UUID> {
+//}
