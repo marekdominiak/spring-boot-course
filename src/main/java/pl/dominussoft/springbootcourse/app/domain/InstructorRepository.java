c746f16e486ea54b069705ed8e6a320611d1d55e
Marek Dominiak <marek.dominiak@mintra.com>
2022-05-26 14:06:18 +0200

    cosmietics

--- src/main/java/pl/dominussoft/springbootcourse/app/domain/InstructorRepository.java
+++ src/main/java/pl/dominussoft/springbootcourse/app/domain/InstructorRepository.java
@@ -2,12 +2,13 @@ package pl.dominussoft.springbootcourse.app.domain;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 
-public interface InstructorRepository {
+public interface InstructorRepository extends PagingAndSortingRepository<Instructor, UUID> {
 
     Iterable<Instructor> findAll();
 
